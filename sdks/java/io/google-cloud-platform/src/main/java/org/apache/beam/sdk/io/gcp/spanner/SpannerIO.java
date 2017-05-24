bcf26119bab90f218142bac80dccc5f191089bea
Mairbek Khadikov <mairbek@google.com>
2017-05-23 17:21:16 -0700

    reads

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/spanner/SpannerIO.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/spanner/SpannerIO.java
@@ -17,23 +17,26 @@
  */
 package org.apache.beam.sdk.io.gcp.spanner;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import com.google.auto.value.AutoValue;
 import com.google.cloud.ServiceFactory;
 import com.google.cloud.ServiceOptions;
-import com.google.cloud.spanner.AbortedException;
-import com.google.cloud.spanner.DatabaseClient;
-import com.google.cloud.spanner.DatabaseId;
-import com.google.cloud.spanner.Mutation;
-import com.google.cloud.spanner.Spanner;
-import com.google.cloud.spanner.SpannerOptions;
+import com.google.cloud.Timestamp;
+import com.google.cloud.spanner.*;
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableList;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.concurrent.ExecutionException;
 import javax.annotation.Nullable;
 import org.apache.beam.sdk.annotations.Experimental;
+import org.apache.beam.sdk.coders.Coder;
+import org.apache.beam.sdk.io.BoundedSource;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.PTransform;
@@ -44,6 +47,7 @@ import org.apache.beam.sdk.util.BackOff;
 import org.apache.beam.sdk.util.BackOffUtils;
 import org.apache.beam.sdk.util.FluentBackoff;
 import org.apache.beam.sdk.util.Sleeper;
+import org.apache.beam.sdk.values.PBegin;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PDone;
 import org.joda.time.Duration;
@@ -99,6 +103,174 @@ public class SpannerIO {
   @Experimental
   public static Write write() {
     return new AutoValue_SpannerIO_Write.Builder().setBatchSize(DEFAULT_BATCH_SIZE).build();
+  }
+
+  @Experimental
+  public static Read read() {
+    return new AutoValue_SpannerIO_Read.Builder().build();
+  }
+
+  @Experimental(Experimental.Kind.SOURCE_SINK)
+  @AutoValue
+  public abstract static class Read extends PTransform<PBegin, PCollection<Struct>> {
+    @Nullable
+    abstract String getProjectId();
+
+    @Nullable
+    abstract String getInstanceId();
+
+    @Nullable
+    abstract String getDatabaseId();
+
+    @Nullable
+    abstract TimestampBound getTimestampBound();
+
+    @Nullable
+    abstract Statement getQuery();
+
+    @Nullable
+    abstract String getTable();
+
+    @Nullable
+    abstract String getIndex();
+
+    @Nullable
+    abstract List<String> getColumns();
+
+    @Nullable
+    abstract KeySet getKeySet();
+
+    abstract Builder toBuilder();
+
+    @Nullable
+    @VisibleForTesting
+    abstract ServiceFactory<Spanner, SpannerOptions> getServiceFactory();
+
+      public Read withTimestamp(Timestamp timestamp) {
+          return withTimestampBound(TimestampBound.ofReadTimestamp(timestamp));
+      }
+
+      public Read withTimestampBound(TimestampBound timestampBound) {
+          return toBuilder().setTimestampBound(timestampBound).build();
+      }
+
+      public Read withTable(String table) {
+          return toBuilder().setTable(table).build();
+      }
+
+      public Read withColumns(String... columns) {
+          return withColumns(Arrays.asList(columns));
+      }
+
+      public Read withColumns(List<String> columns) {
+          return toBuilder().setColumns(columns).build();
+      }
+
+      public Read withQuery(Statement statement) {
+          return toBuilder().setQuery(statement).build();
+      }
+      public Read withQuery(String sql) {
+          return withQuery(Statement.of(sql));
+      }
+
+      @AutoValue.Builder
+    abstract static class Builder {
+      abstract Builder setProjectId(String projectId);
+
+      abstract Builder setInstanceId(String instanceId);
+
+      abstract Builder setDatabaseId(String databaseId);
+
+      abstract Builder setTimestampBound(TimestampBound timestampBound);
+
+      abstract Builder setQuery(Statement statement);
+
+      abstract Builder setTable(String table);
+
+      abstract Builder setIndex(String index);
+
+      abstract Builder setColumns(List<String> columns);
+
+      abstract Builder setKeySet(KeySet keySet);
+
+      @VisibleForTesting
+      abstract Builder setServiceFactory(ServiceFactory<Spanner, SpannerOptions> serviceFactory);
+
+      abstract Read build();
+    }
+
+    SpannerOptions getSpannerOptions() {
+      SpannerOptions.Builder builder = SpannerOptions.newBuilder();
+      if (getServiceFactory() != null) {
+        builder.setServiceFactory(getServiceFactory());
+      }
+      return builder.build();
+    }
+
+    /**
+     * Returns a new {@link SpannerIO.Write} that will write to the specified Cloud Spanner project.
+     *
+     * <p>Does not modify this object.
+     */
+    public Read withProjectId(String projectId) {
+      return toBuilder().setProjectId(projectId).build();
+    }
+
+    /**
+     * Returns a new {@link SpannerIO.Write} that will write to the specified Cloud Spanner
+     * instance.
+     *
+     * <p>Does not modify this object.
+     */
+    public Read withInstanceId(String instanceId) {
+      return toBuilder().setInstanceId(instanceId).build();
+    }
+
+    /**
+     * Returns a new {@link SpannerIO.Write} that will write to the specified Cloud Spanner
+     * database.
+     *
+     * <p>Does not modify this object.
+     */
+    public Read withDatabaseId(String databaseId) {
+      return toBuilder().setDatabaseId(databaseId).build();
+    }
+
+    @Override
+    public void validate(PipelineOptions options) {
+      checkNotNull(
+          getInstanceId(),
+          "SpannerIO.read() requires instance id to be set with withInstanceId method");
+      checkNotNull(
+          getDatabaseId(),
+          "SpannerIO.read() requires database id to be set with withDatabaseId method");
+      checkNotNull(
+          getTimestampBound(),
+          "SpannerIO.read() runs in a read only transaction and requires timestamp to be set "
+              + "with withTimestampBound or withTimestamp method");
+
+      if (getQuery() != null) {
+          // TODO(mairbek): validate query?
+      } else if (getTable() != null) {
+        // Assume read
+        checkNotNull(
+            getColumns(),
+            "For a read operation SpannerIO.read() requires a list of "
+                + "columns to set with withColumns method");
+        checkArgument(
+            !getColumns().isEmpty(),
+            "For a read operation SpannerIO.read() requires a"
+                + " list of columns to set with withColumns method");
+      } else {
+        throw new IllegalArgumentException(
+            "SpannerIO.read() requires configuring query or read " + "operation.");
+      }
+    }
+
+    @Override
+    public PCollection<Struct> expand(PBegin input) {
+      return input.apply(org.apache.beam.sdk.io.Read.from(new SpannerSource(this)));
+    }
   }
 
   /**
@@ -328,6 +500,139 @@ public class SpannerIO {
       builder
           .addIfNotNull(DisplayData.item("instanceId", spec.getInstanceId()).withLabel("Instance"))
           .addIfNotNull(DisplayData.item("databaseId", spec.getDatabaseId()).withLabel("Database"));
+    }
+  }
+
+  private static class SpannerSource extends BoundedSource<Struct> {
+    private final Read config;
+
+    private SpannerSource(Read config) {
+      this.config = config;
+    }
+
+    @Override
+    public List<? extends BoundedSource<Struct>> split(
+        long desiredBundleSizeBytes, PipelineOptions options) throws Exception {
+      // TODO(mairbek): liquid sharding is not supported yet.
+      return ImmutableList.of(this);
+    }
+
+    @Override
+    public long getEstimatedSizeBytes(PipelineOptions options) throws Exception {
+      return 0L;
+    }
+
+    @Override
+    public BoundedReader<Struct> createReader(PipelineOptions options) throws IOException {
+      if (config.getQuery() != null) {
+        return new SpannerQueryReader(this, config);
+      } else if (config.getIndex() != null) {
+        return new SpannerIndexReader(this, config);
+      }
+      return new SpannerReadReader(this, config);
+    }
+
+    @Override
+    public void validate() {}
+
+    @Override
+    public Coder<Struct> getDefaultOutputCoder() {
+      return new StructCoder();
+    }
+  }
+
+  private abstract static class BaseSpannerReader extends BoundedSource.BoundedReader<Struct> {
+    final SpannerSource source;
+    final Read config;
+
+    ResultSet resultSet;
+    Spanner service;
+    DatabaseClient databaseClient;
+    ReadOnlyTransaction readOnlyTransaction;
+
+    BaseSpannerReader(SpannerIO.Read config, SpannerIO.SpannerSource source) {
+      this.config = config;
+      this.source = source;
+    }
+
+    @Override
+    public boolean start() throws IOException {
+      SpannerOptions options = config.getSpannerOptions();
+      service = options.getService();
+      databaseClient =
+          service.getDatabaseClient(
+              DatabaseId.of(
+                  options.getProjectId(), config.getInstanceId(), config.getDatabaseId()));
+      readOnlyTransaction = databaseClient.readOnlyTransaction(config.getTimestampBound());
+      startRead();
+      return true;
+    }
+
+    protected abstract void startRead();
+
+    @Override
+    public boolean advance() throws IOException {
+      return resultSet.next();
+    }
+
+    @Override
+    public Struct getCurrent() throws NoSuchElementException {
+      return resultSet.getCurrentRowAsStruct();
+    }
+
+    @Override
+    public void close() throws IOException {
+      resultSet.close();
+      readOnlyTransaction.close();
+      try {
+        service.closeAsync().get();
+      } catch (InterruptedException | ExecutionException e) {
+        throw new IOException("Cannot properly shut down spanner", e);
+      }
+    }
+
+    @Override
+    public BoundedSource<Struct> getCurrentSource() {
+      return source;
+    }
+  }
+
+  @VisibleForTesting
+  static class SpannerQueryReader extends BaseSpannerReader {
+
+    SpannerQueryReader(SpannerSource source, Read config) {
+      super(config, source);
+    }
+
+    protected void startRead() {
+      resultSet = readOnlyTransaction.executeQuery(config.getQuery());
+    }
+  }
+
+  @VisibleForTesting
+  static class SpannerReadReader extends BaseSpannerReader {
+
+    SpannerReadReader(SpannerSource source, Read config) {
+      super(config, source);
+    }
+
+    protected void startRead() {
+      resultSet =
+          readOnlyTransaction.read(config.getTable(), config.getKeySet(), config.getColumns());
+    }
+  }
+
+  @VisibleForTesting
+  static class SpannerIndexReader extends BaseSpannerReader {
+
+    SpannerIndexReader(SpannerSource source, Read config) {
+      super(config, source);
+    }
+
+    protected void startRead() {
+      resultSet =
+          readOnlyTransaction.readUsingIndex(
+              config.getTable(), config.getIndex(), config.getKeySet(), config.getColumns());
     }
   }
 
