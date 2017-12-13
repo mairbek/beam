93856dbcf3d89903b054dd54ba4c4ee9d11dac4a
Mairbek Khadikov <mairbek@google.com>
2017-12-12 17:02:33 -0800

    Cloud Spanner connector that uses Batch API.
    Change-Id: Ia794033a90372208ec57045dd3a609f38b18f4e3

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/spanner/SpannerIO.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/spanner/SpannerIO.java
@@ -27,6 +27,7 @@ import com.google.cloud.spanner.AbortedException;
 import com.google.cloud.spanner.Key;
 import com.google.cloud.spanner.KeySet;
 import com.google.cloud.spanner.Mutation;
+import com.google.cloud.spanner.PartitionOptions;
 import com.google.cloud.spanner.Spanner;
 import com.google.cloud.spanner.SpannerOptions;
 import com.google.cloud.spanner.Statement;
@@ -47,6 +48,7 @@ import javax.annotation.Nullable;
 import org.apache.beam.sdk.annotations.Experimental;
 import org.apache.beam.sdk.coders.KvCoder;
 import org.apache.beam.sdk.coders.StringUtf8Coder;
+import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.ValueProvider;
 import org.apache.beam.sdk.transforms.ApproximateQuantiles;
 import org.apache.beam.sdk.transforms.Combine;
@@ -208,6 +210,7 @@ public class SpannerIO {
   public static ReadAll readAll() {
     return new AutoValue_SpannerIO_ReadAll.Builder()
         .setSpannerConfig(SpannerConfig.create())
+        .setTimestampBound(TimestampBound.strong())
         .build();
   }
 
@@ -249,6 +252,9 @@ public class SpannerIO {
     @Nullable
     abstract PCollectionView<Transaction> getTransaction();
 
+    @Nullable
+    abstract TimestampBound getTimestampBound();
+
     abstract Builder toBuilder();
 
     @AutoValue.Builder
@@ -256,6 +262,8 @@ public class SpannerIO {
       abstract Builder setSpannerConfig(SpannerConfig spannerConfig);
 
       abstract Builder setTransaction(PCollectionView<Transaction> transaction);
+
+      abstract Builder setTimestampBound(TimestampBound timestampBound);
 
       abstract ReadAll build();
     }
@@ -314,18 +322,20 @@ public class SpannerIO {
       return toBuilder().setTransaction(transaction).build();
     }
 
+    public ReadAll withTimestamp(Timestamp timestamp) {
+      return withTimestampBound(TimestampBound.ofReadTimestamp(timestamp));
+    }
+
+    public ReadAll withTimestampBound(TimestampBound timestampBound) {
+      return toBuilder().setTimestampBound(timestampBound).build();
+    }
+
     @Override
     public PCollection<Struct> expand(PCollection<ReadOperation> input) {
-      List<PCollectionView<Transaction>> sideInputs =
-          getTransaction() == null
-              ? Collections.<PCollectionView<Transaction>>emptyList()
-              : Collections.singletonList(getTransaction());
       return input
-          .apply(Reshuffle.<ReadOperation>viaRandomKey())
-          .apply(
-              "Execute queries",
-              ParDo.of(new NaiveSpannerReadFn(getSpannerConfig(), getTransaction()))
-                  .withSideInputs(sideInputs));
+          .apply("Reshuffle", Reshuffle.<ReadOperation>viaRandomKey())
+          .apply("Read from Cloud Spanner",
+              BatchSpannerRead.create(getSpannerConfig(), getTransaction(), getTimestampBound()));
     }
   }
 
@@ -344,6 +354,9 @@ public class SpannerIO {
     @Nullable
     abstract PCollectionView<Transaction> getTransaction();
 
+    @Nullable
+    abstract PartitionOptions getPartitionOptions();
+
     abstract Builder toBuilder();
 
     @AutoValue.Builder
@@ -356,6 +369,8 @@ public class SpannerIO {
       abstract Builder setTimestampBound(TimestampBound timestampBound);
 
       abstract Builder setTransaction(PCollectionView<Transaction> transaction);
+
+      abstract Builder setPartitionOptions(PartitionOptions partitionOptions);
 
       abstract Read build();
     }
@@ -396,6 +411,12 @@ public class SpannerIO {
     public Read withDatabaseId(ValueProvider<String> databaseId) {
       SpannerConfig config = getSpannerConfig();
       return withSpannerConfig(config.withDatabaseId(databaseId));
+    }
+
+    /** Specifies the Cloud Spanner database. */
+    public Read withHost(String host) {
+      SpannerConfig config = getSpannerConfig();
+      return withSpannerConfig(config.withHost(host));
     }
 
     /** Specifies the Cloud Spanner host. */
@@ -454,8 +475,13 @@ public class SpannerIO {
       return withReadOperation(getReadOperation().withIndex(index));
     }
 
+    public Read withPartitionOptions(PartitionOptions partitionOptions) {
+      return withReadOperation(getReadOperation().withPartitionOptions(partitionOptions));
+    }
+
     @Override
-    public PCollection<Struct> expand(PBegin input) {
+    public void validate(PipelineOptions options) {
+      super.validate(options);
       getSpannerConfig().validate();
       checkArgument(
           getTimestampBound() != null,
@@ -479,16 +505,14 @@ public class SpannerIO {
             "SpannerIO.read() requires configuring query or read operation.");
       }
 
-      PCollectionView<Transaction> transaction = getTransaction();
-      if (transaction == null && getTimestampBound() != null) {
-        transaction =
-            input.apply(
-                createTransaction()
-                    .withTimestampBound(getTimestampBound())
-                    .withSpannerConfig(getSpannerConfig()));
-      }
-      ReadAll readAll =
-          readAll().withSpannerConfig(getSpannerConfig()).withTransaction(transaction);
+    }
+
+    @Override
+    public PCollection<Struct> expand(PBegin input) {
+      ReadAll readAll = readAll()
+          .withSpannerConfig(getSpannerConfig())
+          .withTimestampBound(getTimestampBound())
+          .withTransaction(getTransaction());
       return input.apply(Create.of(getReadOperation())).apply("Execute query", readAll);
     }
   }
